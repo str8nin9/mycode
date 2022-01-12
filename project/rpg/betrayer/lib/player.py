e2843d91cb013da628c1a665f8699c2582028b7a
str8nin9 <str8nin9@gmail.com>
2022-01-12 01:00:46 +0000

    working on player class

--- project/rpg/betrayer/lib/player.py
+++ project/rpg/betrayer/lib/player.py
@@ -10,14 +10,15 @@ import random
 
 #player setup
 class player:
-    def __init__(self, name, p_hp):
-        self.name = name
-        self.hp = p_hp
+    def __init__(self):
         inventory = []
         print("success")
+    name = input("name?\n>")
+    p_hp = 20
 #myplayer = player()
 
 if __name__ == "__main__":
     hero = player()
+    print(f"{player.name} has {player.p_hp} hp!")
     
     #print(player.hp)
